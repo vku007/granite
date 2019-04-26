e86a8478d4770d8f4b2cfe5bff9ad83bd9e69cae
volodymyr.kuznietsov <volodymyr.kuznietsov@healthbi.com>
2019-04-26 19:32:32 +0300

    add spring-to-angular default routing

--- src/main/js/shader-sampler/src/app/app-routing.module.ts
+++ src/main/js/shader-sampler/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {ProgramsComponent} from "./programs/programs.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {TestDetailsComponent} from "./test-details/test-details.component";
-
+//The order of the routes in the configuration matters
 const routes: Routes = [
   { path: '', redirectTo: '/dynamo', pathMatch: 'full' },
   {path: 'programs', component: ProgramsComponent},
   { path: 'dynamo', component: DashboardComponent },
   { path: 'dynamo/detail/:id', component: TestDetailsComponent },
+  { path: '**', redirectTo: '/dynamo', pathMatch: 'full'  }
   ];
 
 @NgModule({
